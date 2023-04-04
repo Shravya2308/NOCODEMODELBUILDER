bed19cd83c7aa6f01fcbf6f096280604a693f169
Shravya2308 <shravyashetty2308@gmail.com>
2023-04-05 02:13:03 +0530

    refactoring done
    refactoring of classification done

--- pages/demo_classification.py
+++ pages/demo_classification.py
@@ -5,6 +5,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
+from models.classification import classifier
+from models.classification import predict
 
 
 
@@ -30,29 +32,12 @@ X = df.iloc[:, :-1].values
 y = df.iloc[:, -1].values 
 
 
+model = classifier(df,n_estimators_o)
 
 # st.write(X)
 # st.write(y)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2 ,random_state=42)
-from sklearn.preprocessing import StandardScaler
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
 
-lc_model = LogisticRegression() #object
-lc_model.fit(X_train, y_train)
-y_pred_l = lc_model.predict(X_test)
-
-
-lc_acc = accuracy_score(y_test, y_pred_l)
-# st.write(lc_acc)     
-
-from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators=st.session_state['var1'], criterion = 'entropy', random_state = 0)
-classifier.fit(X_train, y_train)
-y_pred_c = classifier.predict(X_test)
-classifier_acc = accuracy_score(y_test, y_pred_c)
 X_coloumns  = list(df.iloc[:, :-1].columns.values)
 st.write(X_coloumns)
 a = len(X_coloumns) 
@@ -63,13 +48,13 @@ for i in range(a):
     list_of_variables.append(val)
 
 
-
+prediction = predict(list_of_variables)
 
 st.write(list_of_variables)
 # st.write(type(list_of_variables[0]))
-if classifier_acc>lc_acc:
-    st.write(classifier.predict(sc.transform([list_of_variables])))
+if model['classifier_acc']>model['lc_acc']:
+    st.write(prediction['randomf_predict'])
     st.write('model chosen by ensemble learning==>random_forest')
 else:
-    st.write(lc_model.predict(sc.transform([list_of_variables])))
+    st.write(prediction['logistic_predict'])
     st.write('model chosen by ensemble learning==>logistic')
